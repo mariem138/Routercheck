672455c8fdabecc898ca449b2e4681083f476bf7
mariem138 <79192087+mariem138@users.noreply.github.com>
2021-06-03 13:49:21 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './containers/app';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../style/style.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
